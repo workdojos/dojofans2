e058302d57f28db32a27ad94fa77de3620d3527b
viewprox86 <workmates.live@outlook.com>
2024-01-04 05:30:50 -0600

    er

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ const ACTIONS: Array<NavigationItem> = [
 
 	{
 		type: NavigationItemType.LINK,
+		external: true,
 		href: 'https://youtube.com/@dojofans',
 		icon: <Icon className="mr-3" icon="feather:copy" />,
 		text: 'Youtube',
